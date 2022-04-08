398465fdb535b15e5ca4ab1d594595808bf5d9dc
IoannisCh <ioannis.chantolios@ieee.org>
2022-04-08 22:20:35 +0100

    modified code on welcome script

--- welcome.sh
+++ welcome.sh
@@ -4,8 +4,6 @@ echo "Please enter your name:"
 read name
 
 greating="Welcome"
-user=$(whoami)
 day=$(date +%A)
 
-echo "Welcome $user and $name ! Today is $day !!"
-echo "Your shell version is: $BASH_VERION. Enjoy!"
+echo "Welcome $name ! Today is $day !!"
